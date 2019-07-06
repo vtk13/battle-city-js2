3dd61e78fb677f50fd6f7cff473ff71663bfa33c
Viktor Rogov <vtk666@gmail.com>
2019-07-06 20:26:07 +0300

    set sectors dynamically

--- src/core/server.js
+++ src/core/server.js
@@ -1,9 +1,13 @@
 const EventEmitter = require('events');
 
 class BCServer extends EventEmitter {
+    constructor(sectors){
+        super();
+        this.sectors = sectors;
+    }
     subscribe(sectors){
         for (let sectorId of sectors)
-            this.emit('subscribe', sectorId, [{}, {}]);
+            this.emit('subscribe', sectorId, this.sectors[sectorId]);
     }
 }
 
--- test/core.js
+++ test/core.js
@@ -1,10 +1,12 @@
 const assert = require('assert');
+const BCServer = require('../src/core/server');
 
-let BCServer = require('../src/core/server');
 describe('server', ()=>{
     // player connects to the server and gets sector objects
     it('subscribe', done=>{
-        let server = new BCServer();
+        let server = new BCServer({
+            1: [{}, {}],
+        });
         server.on('subscribe', (sectorId, objects)=>{
             assert.equal(sectorId, 1);
             assert.deepEqual(objects, [{}, {}]);
