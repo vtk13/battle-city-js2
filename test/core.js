dea6561861004d862ab516cf1c639378a325ffac
Viktor Rogov <vtk666@gmail.com>
2019-07-07 08:33:32 +0300

    player receives actual data on connect test in progress

--- test/core.js
+++ test/core.js
@@ -42,4 +42,25 @@ describe('server', ()=>{
         session1.step(234, 1, 'A', [{key: 'w'}]);
         session2.step(234, 1, 'A', [{key: 's'}]);
     });
+    it('player receives actual data on connect', ()=>{
+        let server = new BCServer({
+            1: [{}, {}],
+        });
+        let session1 = server.createSession();
+        session1.on('subscribe', (sectorId, objects)=>{
+            assert.deepEqual(objects, [{}, {}]);
+        });
+        session1.on('getSector', sectorId=>{
+            session1.setSector(sectorId, [{}, {}, {}]);
+        });
+        session1.subscribe([1]);
+        session1.step(234, 1, 'A', [{key: 'w'}]);
+
+        let session2 = server.createSession();
+        session2.on('subscribe', (sectorId, objects)=>{
+            assert.deepEqual(objects, [{}, {}, {}]);
+            done();
+        });
+        session2.subscribe([1]);
+    });
 });
